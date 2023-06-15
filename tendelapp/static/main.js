ae12fad290836a415cd1b83eafcfe630b89f54e1
nishapoudel <nishapoudel400@gmail.com>
2023-06-15 11:29:53 +0545

    fixed

--- tendelapp/static/main.js
+++ tendelapp/static/main.js
@@ -88,6 +88,19 @@ $(window).scroll(function () {
 
 
 
+    // Header carousel
+    $(".header-carousel").owlCarousel({
+        autoplay: true,
+        smartSpeed: 1500,
+        items: 1,
+        dots: false,
+        loop: true,
+        nav : true,
+        navText : [
+            '<i class="bi bi-chevron-left"></i>',
+            '<i class="bi bi-chevron-right"></i>'
+        ]
+    });
 
 
 })(jQuery);
